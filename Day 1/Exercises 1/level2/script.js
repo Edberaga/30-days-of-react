b92d40cd7ad64e7f8eb8056162973f09bcd052fc
Edberaga <edbetjonnathan@gmail.com>
2024-01-09 02:00:30 +0700

    Array exercises

---
+++ Day 1/Exercises 1/level2/script.js
@@ -0,0 +1,6 @@
+/*
+Compare the values of myAge and yourAge using if … else. 
+Based on the comparison and log the result to console stating who is older (me or you). 
+Use prompt(“Enter your age:”) to get the age as input.
+*/
+

d43bf65abacbe4a1bc85bae36846340f49a37a41
Edberaga <edbetjonnathan@gmail.com>
2024-01-09 02:01:57 +0700

    Exercises 2 completed

---
+++ Day3/exercises/level2/level2/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import RenderImage from './component/RenderImage';
+import Subcriber from './component/Subcriber';
+
+function App() {
+  return (
+  <div className="App">
+    <RenderImage width={true}/>
+    <Subcriber />
+  </div>
+  );
+}
+
+export default App;

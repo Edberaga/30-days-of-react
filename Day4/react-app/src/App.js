28de446436c678aa2f67a73e004037f8e550b3f2
Edberaga <edbetjonnathan@gmail.com>
2024-01-12 23:45:39 +0700

    Develop 5 Hexabox

--- Day4/react-app/src/App.js
+++ Day4/react-app/src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import HexaBox from './component/HexaBox';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HexaBox color="#fd8cef" />
+      <HexaBox color="#ef0172" />
+      <HexaBox color="#81ef22" />
+      <HexaBox color="#520ff4" />
+      <HexaBox color="#18cdef" />
     </div>
   );
 }

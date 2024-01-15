5ac3d29aa28a8383ab02a9623b5f248d7ef2552d
Edberaga <edbetjonnathan@gmail.com>
2024-01-15 15:36:08 +0700

    Day 6 react app exercises

---
+++ Day6/react-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import OddEvenBox from './component/OddEvenBox';
+import HexaBox from './component/HexaBox';
+import Population from './component/Population';
+
+function App() {
+  return (
+    <div className='App'>
+      <OddEvenBox/>
+      <HexaBox/>
+      <Population/>
+    </div>
+  );
+}
+
+export default App;

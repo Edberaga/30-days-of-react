7f92cde613bb8c33a03e0b767e7752f97dd7ecd1
Edberaga <edbetjonnathan@gmail.com>
2024-01-18 23:05:46 +0700

    Change backgropund exercises

---
+++ Day8/react-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("black-bg");
+  const changeColor = () => {
+    if(color === "black-bg") {
+      setColor("white-bg");
+    }
+    else {
+      setColor("black-bg");
+    }
+  }
+
+  return (
+   <div className={`App ${color}`}>
+    <h1>State Management Exercises</h1>
+    <button onClick={() => changeColor()}>Change Background</button>
+   </div>
+  )
+}
+
+export default App

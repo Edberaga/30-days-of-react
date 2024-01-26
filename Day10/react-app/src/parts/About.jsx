598aa300416bbda8cf88d00f22687f34335592a1
Edberaga <edbetjonnathan@gmail.com>
2024-01-26 14:53:19 +0700

    About us component

---
+++ Day10/react-app/src/parts/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+  <section className="container" id="about">
+    <div>
+      <h1>About me</h1>
+      <p>I'm just an ordinary developer</p>
+    </div>
+    
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit minus, 
+        debitis quo quas labore numquam, error asperiores, sit eligendi nobis voluptate 
+        neque accusamus quibusdam nostrum voluptatem omnis in quod veniam?
+      </p>
+    </div>
+  </section>
+  )
+}
+
+export default About
